e9291d334cf483c5c69d237c78aa71b74cde9b40
cultureweb <cedric.milius@yahoo.fr>
2019-06-04 11:45:32 +0200

    Premier contenu dynamique

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //    <h1>Hi, i'm a React app</h1>
-    //   </div>
-    // );
-    return React.createElement('div',{className: 'App'}, React.createElement('h1', null,  'Does this work now ?'));
+    return (
+      <div className="App">
+       <h1>Hi, i'm a React app</h1>
+       <p>This is really working!</p>
+       <Person />
+       <Person />
+       <Person />
+      </div>
+    );
+    /**Voilà pourquoi JSX est si important car sans JSX, on farait ceci pour avoir le même resultat */
+    // return React.createElement('div',{className: 'App'}, React.createElement('h1', null,  'Does this work now ?'));
   }
 }
 
