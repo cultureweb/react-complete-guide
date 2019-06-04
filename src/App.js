499edbcf84b4651c8e7a4b909e38a19157e93d7f
cultureweb <cedric.milius@yahoo.fr>
2019-06-04 13:22:37 +0200

    use state and setState()

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked');
+    // DON'T DO THIS: this.state.persons[0].name = 'Cedric';
+    this.setState( {
+      persons: [
+      { name: 'Cedric', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 27 }
+    ]
+    })
+  }
   render() {
     return (
       <div className="App">
        <h1>Hi, i'm a React app</h1>
        <p>This is really working!</p>
-       <Person name="Max" age="28"/>
-       <Person name="Manu" age="29">My Hobbies: Racing</Person>
-       <Person name="Stephanie" age="26"/>
+       <button onClick={this.switchNameHandler}>Switch Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     /**Voilà pourquoi JSX est si important car sans JSX, on farait ceci pour avoir le même resultat */
