4a55223b17d906aee6fbdd63e37717f7d68833c0
ced <cedric.milius@yahoo.fr>
2019-06-09 12:52:11 +0200

    Je veux obtenir l'ID de l'utilisateur à mettre à jour ou l'index dans le tableau, mais maintenant que

--- src/App.js
+++ src/App.js
@@ -26,20 +26,29 @@ export default class App extends Component {
       ]
     })
   }
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex=this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    //Alternative using Object.assign
+    //const person = Object.assign({}, this.state.persons[personIndex]
+      
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  }
+  inputChangeHandler = (event) => {
     this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
+      username: event.target.value
     })
   }
-  // inputChangeHandler = (event) => {
-  //   this.setState({
-  //     username: event.target.value
-  //   })
-  // }
 
   deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice();//MAKE A COPY
@@ -70,7 +79,8 @@ export default class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id} />
+              key={person.id} 
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       );
