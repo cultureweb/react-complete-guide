dbdf5415e80b82e88ddba35f1c7f90e54cf88168
cultureweb <cedric.milius@yahoo.fr>
2019-06-10 08:34:13 +0200

    ras

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+
+ 
+ const app = props =>{}
+ const [ personState, setPersonsState ] = useState({
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value'
+  });
+    return (
+      <div className="App">
+       <h1>Hi, i'm a React app</h1>
+       <p>This is really working!</p>
+       <button onClick={this.switchNameHandler}>Switch Name</button>
+       <Person 
+        name={personsState.persons[0].name} 
+        age={personsState.persons[0].age}
+        />
+       <Person 
+        name={personsState.persons[0].name} 
+        age={personsStatee.persons[0].age}
+        />
+       <Person 
+        name={personsState.persons[1].name} 
+        age={personsState.persons[1].age}>
+        My Hobbies: Racing
+        </Person>
+       <Person 
+        name={personsState.persons[2].name} 
+        age={personsState.persons[2].age}
+        />
+      </div>
+    );
+    
+    /**Voilà pourquoi JSX est si important car sans JSX, on farait ceci pour avoir le même resultat */
+    // return React.createElement('div',{className: 'App'}, React.createElement('h1', null,  'Does this work now ?'));
   }
+
+
+export default App;
+
+
 
   switchNameHandler = () => {
     // console.log('Was clicked');
@@ -23,20 +58,3 @@ class App extends Component {
     ]
     })
   }
-  render() {
-    return (
-      <div className="App">
-       <h1>Hi, i'm a React app</h1>
-       <p>This is really working!</p>
-       <button onClick={this.switchNameHandler}>Switch Name</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-      </div>
-    );
-    /**Voilà pourquoi JSX est si important car sans JSX, on farait ceci pour avoir le même resultat */
-    // return React.createElement('div',{className: 'App'}, React.createElement('h1', null,  'Does this work now ?'));
-  }
-}
-
-export default App;
