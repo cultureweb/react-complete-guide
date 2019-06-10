8540bf0b3b3471744bd73b191c43eb4e642e5791
ced <cedric.milius@yahoo.fr>
2019-06-10 21:47:03 +0200

    npm run eject modify test: cssRegex in wepack.config.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,16 +1,16 @@
 import React from "react";
-import './Person.css';
+import classes from './Person.css';
 
 const person = props => {
 
   return (
-      <div className="Person" >
-        <p onClick={props.click}>
-          I'm a {props.name} and I am {props.age} years old!{" "}
-        </p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name} />
-      </div>
+    <div className={classes.Person} >
+      <p onClick={props.click}>
+        I'm a {props.name} and I am {props.age} years old!{" "}
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
   );
 };
 
