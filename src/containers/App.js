c1267bee89990ba6d14bb5b5b54ee6366281093b
ced <cedric.milius@yahoo.fr>
2019-06-17 20:50:21 +0200

    90. Component Update Lifecycle (for state Changes)

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,29 +1,44 @@
-import React, { Component } from 'react'
-import classes from './App.css'
+import React, { Component } from 'react';
+
+import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
-    username: 'superman',
     persons: [
-      { id: 'gfts', name: 'Max', age: 28 },
-      { id: 'aszr', name: 'Manu', age: 29 },
-      { id: 'kjkl', name: 'Stephanie', age: 26 }
+      { id: 'asfa1', name: 'Max', age: 28 },
+      { id: 'vasdf1', name: 'Manu', age: 29 },
+      { id: 'asdf11', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
   }
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    })
+
+  // componentWillMount() {
+  //   console.log('[App.js] componentWillMount');
+  // }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
+  shouldComponentUpdate() {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+
+  }
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
   }
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -33,8 +48,8 @@ class App extends Component {
     const person = {
       ...this.state.persons[personIndex]
     };
-    //Alternative using Object.assign
-    //const person = Object.assign({}, this.state.persons[personIndex]
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
 
     person.name = event.target.value;
 
@@ -42,49 +57,47 @@ class App extends Component {
     persons[personIndex] = person;
 
     this.setState({ persons: persons });
-  }
-  inputChangeHandler = (event) => {
-    this.setState({
-      username: event.target.value
-    })
-  }
+  };
 
-  deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice();//MAKE A COPY
-    const persons = [...this.state.persons];//Spread Operator ES6 Features
-    persons.splice(personIndex, 1);// EDIT THIS TO NEW ONE
-    this.setState({ persons: persons });// UPDATE THE NEW ARRAY
-  }
-  togglePersonHandler = () => {
+  deletePersonHandler = personIndex => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
+  togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow })
-  }
+    this.setState({ showPersons: !doesShow });
+  };
 
   render() {
+    console.log('[App.js] render');
     let persons = null;
 
-
     if (this.state.showPersons) {
-      persons = <Persons
-        persons={this.state.persons}
-        clicked={this.deletePersonHandler}
-        changed={this.nameChangedHandler} />;
-
+      persons = (
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />
+      );
     }
 
     return (
       <div className={classes.App}>
         <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
-          clicked={this.togglePersonHandler}
+          clicked={this.togglePersonsHandler}
         />
         {persons}
-
       </div>
-
-    )
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
-export default App;
 
+export default App;
