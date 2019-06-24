ccecd96f884950a6f69e116e3905607b40408f3a
ced <cedric.milius@yahoo.fr>
2019-06-24 20:45:45 +0200

    Optimizing Functional with react memo()

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -11,7 +11,12 @@ class Persons extends Component {
     // }
     shouldComponentUpate(nextProps, nextState) {
         console.log('[Persons.js] shouldComponentUpate');
-        return true;
+        if (nextProps.persons !== this.props.persons){
+            return true;
+        } else {
+            return false;
+        } 
+        //return true;
     }
 
     getSnapshotBeforeUpdate(prevProps, prevState) {
